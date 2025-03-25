063273ca0cf36bb5f01c64cdcb71686e8edf3e79
JuanCastillo29 <Juan.castillo.sanz@gmail.com>
2025-03-25 11:00:48 +0100

    Added random number generator and Monte Carlo update

--- main.c
+++ main.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "main.h"
+
+//Generador de números de Parisi-Rapuano.
+void ini_ran(int SEMILLA)
+{
+    int INI,FACTOR,SUM,i;
+    srand(SEMILLA);
+    INI=SEMILLA;
+    FACTOR=67397;
+    SUM=7364893;
+    for(i=0;i<256;i++)
+    {
+        INI=(INI*FACTOR+SUM);
+        irr[i]=INI;
+    }
+    ind_ran=ig1=ig2=ig3=0;
+}
+
+float Random(void)
+{
+    float r;
+    ig1=ind_ran-24;
+    ig2=ind_ran-55;
+    ig3=ind_ran-61;
+    irr[ind_ran]=irr[ig1]+irr[ig2];
+    ir1=(irr[ind_ran]^irr[ig3]);
+    ind_ran++;
+    r=ir1*NormRANu;
+    return r;
+}
+
+//Generador de los números aleatorios según distribución gaussiana por el método de Box-Muller
+double DistrGauss(void){
+    double d1, d2;
+    //Se generan los numeros planos necesarios
+    d1 = Random();
+    d2 = Random();
+    if(d1 == 0)
+        d1 = d2;
+    //Se usa la fórmula para crear dos números independientes según una gaussiana
+    return -sqrt(-2*log(d1))*cos(2*Pi*d2);
+}
 
 void Input( int *input, double *TMax, double *TMin){
     FILE *file = fopen("Input.txt", "r");
@@ -11,9 +53,9 @@ void Input( int *input, double *TMax, double *TMin){
     }
     /*Input order: Lattice size, Number of temperatures which are going to be simulated,
     number of Monte Carlo steps between replica exchanges, number of Monte carlo steps between measures
-    Total number of Monte Carlo steps, maximum temperature, minimum temperature.*/
-    if (fscanf(file, "%d\t%d\t%d\t%d\t%d\t%lf\t%lf\n", (input), (input +1), (input + 2), (input +3), (input + 4),
-                TMax, TMin) != 7){
+    Total number of Monte Carlo steps, maximum temperature, minimum temperature, seed of random number generator*/
+    if (fscanf(file, "%d\t%d\t%d\t%d\t%d\t%lf\t%lf %d\n", (input), (input +1), (input + 2), (input +3), (input + 4),
+                TMax, TMin, (input + 5)) != 8){
         printf("Error reading input parameters.\n");
         fclose(file);
         exit(EXIT_FAILURE);
@@ -30,9 +72,14 @@ void InicializarSistema(EspinLattice *sistema, int L){
         printf("Error at memory allocation.\n");
         exit(1);
     }
+    int spinvalue;
     //Starts with a random configuration.
     for (int i = 0; i < N; i++) {
-        sistema->lattice[i].value = (rand() % 2) ? 1 : -1;
+        if(Random()<0.5)
+            spinvalue = -1;
+        else
+            spinvalue = +1;
+        sistema->lattice[i].value = spinvalue;
     }
 
     for (int i = 0; i < L; i++) {
@@ -52,11 +99,43 @@ void FreeSystemMemory(EspinLattice *sistema) {
     free(sistema->lattice);
 }
 
+void Actualizarvecinos(Espin *spin, Espin *vecino, double Beta){
+
+}
+
+void PasoMonteCarlo(EspinLattice *sistema){
+    int n;
+    Espin spin;
+
+    for(int i=0; i< sistema->Size; i++){
+        n = Random()*sistema->Size;
+        spin = sistema->lattice[n];
+
+        //With a probability given by the Metropolis algorithm, I change the spin.
+        if(Random()< spin.prob){
+
+            //The change in the spin also changes the field that its neighbours see.
+            for(int j=0; j<4; j++)
+                Actualizarvecinos(&spin, &sistema->lattice[spin.vecinos[j]], sistema->Beta);
+
+            //I change the system
+            sistema->Energy += spin.dE;
+            spin.value = -spin.value;
+            spin.dE  = -spin.dE;
+        }
+    }
+}
+
+void CambioTemperatura(EspinLattice *sistema1, EspinLattice *sistema2){
+
+}
+
 int main()
 {
-    int inputdata[5];
+    int inputdata[6];
     double TMax, TMin;
     Input(inputdata, &TMax, &TMin);
+    ini_ran(inputdata[5]);
 
     EspinLattice *sistemas = (EspinLattice *)malloc(2*inputdata[1]* sizeof(EspinLattice));
     if (sistemas == NULL) {
--- main.h
+++ main.h
@@ -1,15 +1,26 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
+unsigned int irr[256];
+unsigned int ir1;
+unsigned char ind_ran,ig1,ig2,ig3;
+#define NormRANu (2.3283063671E-10F)
+#define Pi 3.14159265
+
+
 typedef struct{
     int value;
     int vecinos[4];
+    double dE;
+    double prob;
 }Espin;
+
 
 typedef struct{
     Espin *lattice;
     int Size;
     double Beta;
+    double Energy;
 }EspinLattice;
 
 void Input( int *input, double *TMax, double *TMin);
